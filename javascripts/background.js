97611544c5260ab23b872429db3adef9973eea6e
Malte <malte.luttermann@student.uni-luebeck.de>
2019-05-10 07:45:22 +0200

    First algorithm selection design idea

--- javascripts/background.js
+++ javascripts/background.js
@@ -8,20 +8,27 @@ var browserHistory = new Map();
 
 /*
  * Milliseconds to subtract from the current time in order to get the start time for the browser
- * history. The default value is set to the last 3 days and the user can change the value at any
+ * history. The default value is set to the last 24 hours and the user can change the value at any
  * time.
  */
-var interval = 1000 * 60 * 60 * 24 * 3;
+var interval = 1000 * 60 * 60 * 24;
+
+/*
+ * The maximum amount of connections to be made.
+ */
+var maxConnectionCount = 15;
 
 /*
  * Specifies which algorithms for fooling fingerprinters are available.
  *
- * algorithms.DEFAULT: algorithms.HISTORY is the default algorithm at the moment.
- * algorithms.HISTORY: Uses the browser history to visit websites from there randomly.
+ * algorithms.DEFAULT: Opens a new tab, but does not click on links or search for keywords.
+ * algorithms.NAVIGATE: Opens a new tab and clicks on links in that tab.
+ * algorithms.SEARCH: Opens a new tab and searches for keywords in that tab.
  */
 const algorithms = {
 	DEFAULT: 'DEFAULT',
-	HISTORY: 'HISTORY'
+	NAVIGATE: 'NAVIGATE',
+	SEARCH: 'SEARCH'
 };
 
 /*
@@ -37,18 +44,20 @@ var visitedSitesCount, clickedLinksCount, keywordSearchCount;
 
 /**
  * Starts the application: Creates fake connections in the hidden window and removes the tabs
- * when finished. The selected algorithm defines what these fake connections do exactly.
+ * when finished. The selected algorithm defines what exactly these fake connections do.
  */
 function runApplication() {
-	initialize();
-	selectAlgorithm(); // Saves selected algorithm in variable 'activeAlgorithm'
+	initialize(); // Saves selected algorithm in variable 'activeAlgorithm'
 
 	switch (activeAlgorithm) {
 		case algorithms.DEFAULT:
 			execDefault();
 			break;
-		case algorithms.HISTORY:
-			execHistory();
+		case algorithms.NAVIGATE:
+			execNavigate();
+			break;
+		case algorithms.SEARCH:
+			execSearch();
 			break;
 	}
 };
@@ -57,6 +66,8 @@ function runApplication() {
  * Loads the statistics and settings from the storage at the start of the application.
  */
 function initialize() {
+	// Only necessary when user clicks on the extension page icon. Therefore, we do not need to
+	// put the following instructions into the callback function.
 	chrome.storage.sync.get([
 		'visitedSitesCount', 'clickedLinksCount', 'keywordSearchCount'
 	], function (res) {
@@ -64,33 +75,9 @@ function initialize() {
 		clickedLinksCount = res.clickedLinksCount != undefined ? res.clickedLinksCount : 0;
 		keywordSearchCount = res.keywordSearchCount != undefined ? res.keywordSearchCount : 0;
 	});
-}
 
-/**
- * Selects an algorithm to execute based on some criteria (not defined yet).
- */
-function selectAlgorithm() {
-	// TODO: Implementation & Update comment
-}
-
-/**
- * Executes the default algorithm.
- * 
- * This algorithm does nothing, apparently. Therefore, it calls another algorithm for now.
- */
-function execDefault() {
-	// TODO: Implementation & Update comment & Update comment for algorithms constant
-	execHistory();
-}
-
-/**
- * Executes the history algorithm.
- * 
- * This algorithm creates connections based on the users browser history so far.
- */
-function execHistory() {
 	chrome.history.search({
-		'text': '',
+		'text': '', // All entries in a given time interval
 		'startTime': (new Date).getTime() - interval
 	}, function (historyItems) {
 		// Save which urls were visited and how often they were visited (using a key-value
@@ -104,19 +91,58 @@ function execHistory() {
 			return a[1] - b[1];
 		}));
 
-		var index = 0;
-		var connectionCount = 5;
-		for (const [key, value] of browserHistory.entries()) {
-			setTimeout(function () {
-				connectToUrl('https://google.de', algorithms.HISTORY);
-			}, 1000);
+		// var index = 0;
+		// var connectionCount = 5;
+		// for (const [key, value] of browserHistory.entries()) {
+		// 	setTimeout(function () {
+		// 		connectToUrl('https://google.de', algorithms.NAVIGATE);
+		// 	}, 1000);
 
-			index++;
-			if (index == connectionCount) {
-				break;
-			}
+		// 	index++;
+		// 	if (index == connectionCount) {
+		// 		break;
+		// 	}
+		// }
+		for (var i = 0; i < [...browserHistory.keys()].length; i++) {
+			console.log([...browserHistory.keys()][i]);
 		}
 	});
+}
+
+/**
+ * Selects an algorithm to execute based on some criteria (not defined yet).
+ */
+function selectAlgorithm() {
+	// TODO: Implementation & Update comment, be careful with asynchronous functions
+	console.log(browserHistory);
+}
+
+/**
+ * Executes the default algorithm.
+ * 
+ * This algorithm simply opens a webpage.
+ */
+function execDefault() {
+	// TODO: Implementation & Update comment & Update comment for algorithms constant
+
+}
+
+/**
+ * Executes the navigate algorithm.
+ * 
+ * This algorithm navigates to a new url by clicking on links.
+ */
+function execNavigate() {
+
+}
+
+/**
+ * Executes the search algorithm.
+ * 
+ * This algorithm searches for keywords on a webpage.
+ */
+function execSearch() {
+
 }
 
 /**

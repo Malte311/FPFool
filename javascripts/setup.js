b944f139f8c4e4e04c7b500d3bd27b25fa408a74
Malte <malte.luttermann@student.uni-luebeck.de>
2019-06-03 15:20:05 +0200

    Auto format reversed.

--- javascripts/setup.js
+++ javascripts/setup.js
@@ -49,38 +49,39 @@ fetch(dataPath).then(response => response.json()).then(function (json) {
 	/*
 	 * Keep track of third party requests, so we can visit these sites if we want to.
 	 */
-	chrome.webRequest.onBeforeRequest.addListener(function (det) {
-		const excludedTypes = ['stylesheet', 'image']; // Reduce unnecessary effort
-		if (!excludedTypes.includes(det.type) && det.initiator != undefined) {
-			// We are only interested in third party sites, so we ignore first party requests.
-			// (otherwise we would get way too many requests to consider)
-			var startInd = det.initiator.indexOf('.') + 1;
-			var urlExtension = det.initiator.match(/\.[a-z]{2,3}($|\/)/);
-			var endInd = urlExtension != null ? det.initiator.indexOf(urlExtension[0]) : -1;
-			if (endInd > 0 && !det.url.includes(det.initiator.substring(startInd, endInd))) {
-				var val = thirdParties.get(det.initiator);
-				thirdParties.set(
-					det.initiator,
-					val != undefined ?
+	chrome.webRequest.onBeforeRequest.addListener(
+		function (det) {
+			const excludedTypes = ['stylesheet', 'image']; // Reduce unnecessary effort
+			if (!excludedTypes.includes(det.type) && det.initiator != undefined) {
+				// We are only interested in third party sites, so we ignore first party requests.
+				// (otherwise we would get way too many requests to consider)
+				var startInd = det.initiator.indexOf('.') + 1;
+				var urlExtension = det.initiator.match(/\.[a-z]{2,3}($|\/)/);
+				var endInd = urlExtension != null ? det.initiator.indexOf(urlExtension[0]) : -1;
+				if (endInd > 0 && !det.url.includes(det.initiator.substring(startInd, endInd))) {
+					var val = thirdParties.get(det.initiator);
+					thirdParties.set(
+						det.initiator,
+						val != undefined ?
 						(val.includes(det.url) ? val : val.concat([det.url])) : [det.url]
-				);
+					);
 
-				// Find other websites that use the same third party and are not added to the
-				// queue yet
-				for (const [key, val] of thirdParties) {
-					if (!key.includes(det.initiator.substring(startInd, endInd)) &&
-						val.includes(det.url) && !queue.includes(key)) {
-						queue.push(key);
-						// Restart loop if queue was empty before and maximum number of
-						// connections is not reached yet.
-						if (!(queue.length > 1) && todayConnectionCount < connectionLimit) {
-							restartLoop(5000 * Math.random() + 10000); // 10 to 15 seconds
+					// Find other websites that use the same third party and are not added to the
+					// queue yet
+					for (const [key, val] of thirdParties) {
+						if (!key.includes(det.initiator.substring(startInd, endInd)) &&
+							val.includes(det.url) && !queue.includes(key)) {
+							queue.push(key);
+							// Restart loop if queue was empty before and maximum number of
+							// connections is not reached yet.
+							if (!(queue.length > 1) && todayConnectionCount < connectionLimit) {
+								restartLoop(5000 * Math.random() + 10000); // 10 to 15 seconds
+							}
 						}
 					}
 				}
 			}
-		}
-	}, {
+		}, {
 			urls: ['http://*/*', 'https://*/*']
 		},
 		['requestBody']
